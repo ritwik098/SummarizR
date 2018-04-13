d21f9b7f943b21d79eb516bbceace03098d28d29
Nisarg Kolhe <nisargkolhe@gmail.com>
2018-04-13 13:19:01 -0400

    Set up frontend routing

---
+++ backend/frontend/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent }   from './home/home.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+
+const routes: Routes = [
+	{ path: '', component: HomeComponent },
+	{ path: 'dashboard', component: DashboardComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes, {useHash: true}) ],
+	exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
--- backend/frontend/src/app/app.module.ts
+++ backend/frontend/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule, Http, Headers, Response, RequestOptions } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    HttpClientModule
+	],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
