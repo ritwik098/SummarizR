87cf2e50512767a814cd851d98fe8a3035bfce62
Nisarg Kolhe <nisargkolhe@gmail.com>
2018-04-21 09:41:23 -0400

    Implemented pastNotes

--- backend/frontend/src/app/notes/notes.component.ts
+++ backend/frontend/src/app/notes/notes.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../auth.service'
+
+import { User } from '../user';
+
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotesComponent implements OnInit {
 
-  constructor() { }
+	user: User;
+	notes = new Map<string, any>();
+	currentNote: any = {};
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  	this.user = this.authService.loadUserFromLocalStorage();
+  	for(let note of this.user.pastNotes){
+  		this.notes.set(note.title, note);
+  	}
+  	console.log(this.notes);
   }
+
+  getKeys(map){
+    return Array.from(map.keys());
+  }
+
+  selectNote(title: string){
+  	this.currentNote = this.notes.get(title);
+  	console.log(this.currentNote);
+  }
+
+  jumpTo(time: number){
+		var video = <HTMLVideoElement>document.getElementById("mainVideo");
+		video.currentTime = time;
+	}
+
+  getTime(time: number){
+		let number = Math.round(time);
+		let minutes = Math.floor(number / 60);
+		let seconds = number % 60;
+
+		return "" + minutes + ":" + seconds;
+	}
 
 }
